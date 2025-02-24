e5a82c4f5006289245921bd0c0c14ab954216cca
Tan Chin Siang <chinsiang99@gmail.com>
2025-02-24 16:48:10 +0800

    feat: working with jsx rules

---
+++ project-01/src/components/JSXRules.jsx
@@ -0,0 +1,12 @@
+const JSXRules = () => {
+  return (
+    <div>
+        <h1>JSX Rules</h1>
+        <p className="rule-1">jsx rules 1 here</p>
+        <p className="rule-2">jsx rules 2 here</p>
+        <p className="rule-3">jsx rules 3 here</p>
+    </div>
+  )
+}
+
+export default JSXRules
