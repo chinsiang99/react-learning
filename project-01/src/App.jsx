577b80cffd93df9d94d5f48ce38d8294d6586ef1
Tan Chin Siang <chinsiang99@gmail.com>
2025-02-24 16:52:53 +0800

    feat: working with jsx rules

--- project-01/src/App.jsx
+++ project-01/src/App.jsx
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import JSXRules from "./components/JSXRules"
 import MainContent from "./components/MainContent"
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <Header/>
       <MainContent/>
+      <JSXRules/>
       <Footer/>
     </div>
   )
