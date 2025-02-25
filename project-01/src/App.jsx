5d487b8959a5b179a02c7b14a6923cbaca53ee15
Tan Chin Siang <chinsiang99@gmail.com>
2025-02-25 08:58:59 +0800

    feat: working with props

--- project-01/src/App.jsx
+++ project-01/src/App.jsx
@@ -4,6 +4,7 @@ import JSXRules from "./components/JSXRules"
 import MainContent from "./components/MainContent"
 import NumbersList from "./components/NumbersList"
 import Person from "./components/Person"
+import Product from "./components/Product"
 import ProductInfo from "./components/ProductInfo"
 import UserList from "./components/UserList"
 
@@ -17,6 +18,7 @@ const App = () => {
       <NumbersList/>
       <UserList/>
       <Person name={"chin siang"} age={26}/>
+      <Product name={"Mouse"} price={99}/>
       <Footer/>
     </div>
   )
