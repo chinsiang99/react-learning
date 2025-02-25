9f964b2cf90e7be1bc2bb7570f67a395ce0ba5ef
Tan Chin Siang <chinsiang99@gmail.com>
2025-02-25 08:47:10 +0800

    feat: working with lists

--- project-01/src/App.jsx
+++ project-01/src/App.jsx
@@ -4,6 +4,7 @@ import JSXRules from "./components/JSXRules"
 import MainContent from "./components/MainContent"
 import NumbersList from "./components/NumbersList"
 import ProductInfo from "./components/ProductInfo"
+import UserList from "./components/UserList"
 
 const App = () => {
   return (
